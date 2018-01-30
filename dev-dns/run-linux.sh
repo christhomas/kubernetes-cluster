36972e0a99ba6e99d4c9c733a9f705c30cb7ba60
Chris Thomas <chris.thomas@antimatter-studios.com>
2018-01-30 14:31:27 +0100

    Implement a dnsmasq service which can resolve local docker/kubernetes domains and create a linux script to implement it

---
+++ dev-dns/run-linux.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+docker-compose stop
+docker-compose up -d
+
+resolve_conf=/etc/resolvconf/resolv.conf.d/head
+container_name="jpillora/dnsmasq:latest"
+container_id=$(docker ps | grep $container_name | awk '{ print $1 }')
+ipaddress=$(docker inspect -f '{{ range .NetworkSettings.Networks }}{{ .IPAddress }}{{ end }}' $container_id)
+
+echo "Updating DNS Resolver to use container id '$container_id' with ip address '$ipaddress'"
+echo "Note: If you are asked for your password, it means your sudo password"
+
+# I wanted to use a variable here, but the special characters defeated me :(
+sudo sed -i "/\# CONTAINER\:jpillora\/dnsmasq\:latest/d" $resolve_conf
+
+echo "nameserver $ipaddress # CONTAINER:$container_name ip address" | sudo tee -a $resolve_conf
+sudo resolvconf -u
+cat $resolve_conf
